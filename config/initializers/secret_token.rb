3c7520838e0d5c20d49fac3ba9b18aa002787955
Steven Lu <tacticalazn@gmail.com>
2013-01-28 13:17:11 -0500

    New Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linkr::Application.config.secret_token = 'e5b2d281acf3828fd3f494a73ec998f6cccbdf13f59be8ec2353d9a9e4db2c448abc83b604ba064c00c5a7ef26deb5894bf201fc4809180553d9645d65594f57'
