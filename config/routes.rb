cb22f0fab6a41a985c24eb67874e8d2cf7ac824b
Steven Lu <tacticalazn@gmail.com>
2013-01-28 13:35:17 -0500

    Added in some scaffolding :)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Linkr::Application.routes.draw do
+  resources :links
+
+  # Defaults
+  match '/' => 'links#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
